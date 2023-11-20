9861102cf7b90aa8a1946449fef00ac2e1c5af68
Vladimir Bykov <Voviihb@gmail.com>
2023-11-20 16:50:46 +0300

    Initial commit

---
+++ app/src/main/java/com/voviihb/dz2/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.voviihb.dz2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.voviihb.dz2.ui.theme.DZ2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
