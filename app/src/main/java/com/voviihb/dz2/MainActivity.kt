59e68875406c9468be9eccef8d49d47689205ca4
Vladimir Bykov <Voviihb@gmail.com>
2023-11-21 13:02:03 +0300

    Changed LiveData to Kotlin`s Flow

--- app/src/main/java/com/voviihb/dz2/MainActivity.kt
+++ app/src/main/java/com/voviihb/dz2/MainActivity.kt
@@ -15,39 +15,61 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    lateinit var viewModel: MainViewModel
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val mainRepository = MainRepository(ApiFactory.apiService)
         viewModel =
             ViewModelProvider(this, MyViewModelFactory(mainRepository))[MainViewModel::class.java]
 
-        viewModel.loading.observe(this) {
-            if (it) {
-                Log.d(TAG, "LOADING")
-            } else {
-                Log.d(TAG, "STOP LOADING")
-            }
+        val dogsList: MutableList<DogImage?> = mutableListOf()
+
+        this.lifecycleScope.launch {
+            viewModel.dogsImage
+                .flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
+                .distinctUntilChanged()
+                .collect { data ->
+                    dogsList.add(data)
+                    Log.d(TAG, data.toString())
+                }
         }
 
-        val dogsList: MutableList<DogImage> = mutableListOf()
-        viewModel.dogsImage.observe(this) {
-            dogsList.add(it)
-            Log.d(TAG, dogsList.toString())
+        this.lifecycleScope.launch {
+            viewModel.loading
+                .flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
+                .distinctUntilChanged()
+                .collect {
+                    if (it) {
+                        Log.d(TAG, "LOADING")
+                    } else {
+                        Log.d(TAG, "STOP LOADING")
+                    }
+                }
         }
+
 
         viewModel.loadDogImages()
 
 
         setContent {
+            val list = remember {
+                mutableStateListOf(dogsList)
+            }
             Column(
                 modifier = Modifier.fillMaxSize(),
                 verticalArrangement = Arrangement.SpaceBetween,
@@ -57,8 +79,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxWidth(),
                     contentPadding = PaddingValues(16.dp)
                 ) {
-                    items(dogsList) {
-                        ListRow(model = it)
+                    items(list[0]) {
+                        if (it != null) ListRow(model = it)
                     }
                     item {
                         Text("CHEEECk")
@@ -70,7 +92,6 @@ class MainActivity : ComponentActivity() {
                     Button(modifier = Modifier.fillMaxWidth(),
                         onClick = {
                             viewModel.loadDogImages()
-                            Log.d(TAG, dogsList.toString())
                         }
                     ) {
                         Text("Button push")
--- app/src/main/java/com/voviihb/dz2/MainViewModel.kt
+++ app/src/main/java/com/voviihb/dz2/MainViewModel.kt
@@ -1,31 +1,40 @@
 package com.voviihb.dz2
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 const val TAG = "MainViewModel"
 class MainViewModel constructor(private val mainRepository: MainRepository) : ViewModel() {
-    val errorMessage = MutableLiveData<String>()
-    val dogsImage = MutableLiveData<DogImage>()
-    var job: Job? = null
-    val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+    private val _errorMessage = MutableStateFlow<String?>(null)
+    val errorMessage: StateFlow<String?> = _errorMessage
+
+    private val _dogsImage = MutableStateFlow<DogImage?>(null)
+    val dogsImage: StateFlow<DogImage?> = _dogsImage
+
+    private val _loading = MutableStateFlow(false)
+    val loading: StateFlow<Boolean> = _loading
+
+    private var job: Job? = null
+
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
         onError("Exception handled: ${throwable.localizedMessage}")
     }
-    val loading = MutableLiveData<Boolean>()
+
     fun loadDogImages() {
-        loading.value = true
+        _loading.value = true
         job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
             val response = mainRepository.loadDogImage()
             withContext(Dispatchers.Main) {
                 if (response.isSuccessful) {
-                    dogsImage.postValue(response.body())
-                    loading.value = false
+                    _dogsImage.value = response.body()
+                    _loading.value = false
                 } else {
                     onError("Error : ${response.message()} ")
                 }
@@ -34,8 +43,8 @@ class MainViewModel constructor(private val mainRepository: MainRepository) : Vi
     }
 
     private fun onError(message: String) {
-        errorMessage.value = message
-        loading.value = false
+        _errorMessage.value = message
+        _loading.value = false
     }
 
     override fun onCleared() {
